3cb92357c30f54f863133aa84f5eade6c05ff068
Jhall118 <57575958+Jhall118@users.noreply.github.com>
2020-01-01 21:17:26 -0800

    Update setupCAN.sh

--- setupCAN.sh
+++ setupCAN.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+sudo busybox devmem 0x0c303000 32 0x0000C400
+sudo busybox devmem 0x0c303008 32 0x0000C458
+sudo busybox devmem 0x0c303010 32 0x0000C400
+sudo busybox devmem 0x0c303018 32 0x0000C458
 sudo modprobe can
 sudo modprobe can_raw
 sudo modprobe mttcan
