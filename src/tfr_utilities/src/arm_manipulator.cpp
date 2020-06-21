8344b1bd125857dd00d58a595afa27b6d43e23f8
Daniel Mamaghani <daniel.mamaghani@gmail.com>
2020-06-20 19:29:46 -0700

    Add current time into JointState messages sent by moveArmWithoutPlanningOrLimits().

--- src/tfr_utilities/src/arm_manipulator.cpp
+++ src/tfr_utilities/src/arm_manipulator.cpp
@@ -94,13 +94,18 @@ void ArmManipulator::moveArmWithoutPlanningOrLimits(
     sensor_msgs::JointState upper_arm_joint_state;
     sensor_msgs::JointState scoop_joint_state;
 
+    auto current_time = ros::Time::now();
+
     //lower_arm_joint_state.name.push_back("/device23/set_joint_state");
+    lower_arm_joint_state.header.stamp = current_time;
     lower_arm_joint_state.position.push_back(lower_arm);
 
     //upper_arm_joint_state.name.push_back("/device45/set_joint_state");
+    upper_arm_joint_state.header.stamp = current_time;
     upper_arm_joint_state.position.push_back(upper_arm);
 
     //scoop_joint_state.name.push_back("/device56/set_joint_state");
+    scoop_joint_state.header.stamp = current_time;
     scoop_joint_state.position.push_back(scoop);
 
     lower_arm_publisher.publish(lower_arm_joint_state);
