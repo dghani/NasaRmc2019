614db9528b6e0e0ead8e438a28b220b552854bad
Jhall118 <57575958+Jhall118@users.noreply.github.com>
2020-07-08 21:50:06 -0700

    Remove unnecessary code
    Trying to reduce the code to only what is needed by removing devices that will not be used in this years competition robot.

--- src/tfr_can/src/create_ros_topics_for_can_nodes.cpp
+++ src/tfr_can/src/create_ros_topics_for_can_nodes.cpp
@@ -26,56 +26,54 @@ const double loop_rate = 10; // 10 Hz
 const int slow_loop_rate = 1; // 1 Hz
 
 // CANopen node IDs:
-const int IMU_NODE_ID = 120;
 const int SERVO_CYLINDER_LOWER_ARM = 23;
-const int SERVO_CYLINDER_SPARE 	= 34;
 const int SERVO_CYLINDER_UPPER_ARM = 45;
 const int SERVO_CYLINDER_SCOOP = 56;
 const int TURNTABLE = 1;
 const int SERVO_CYLINDER_BIN_LEFT = 77; 
 const int SERVO_CYLINDER_BIN_RIGHT = 88; 
 
-void setupDevice4Topics(kaco::Device& device, kaco::Bridge& bridge, std::string& eds_files_path){
+//void setupDevice4Topics(kaco::Device& device, kaco::Bridge& bridge, std::string& eds_files_path){
     // Roboteq SDC3260 in Closed Loop Count Position mode.
 
-	auto iosub_4_1_1 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_cango/cmd_cango_1");
-	bridge.add_subscriber(iosub_4_1_1);
+	//auto iosub_4_1_1 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_cango/cmd_cango_1");
+	//bridge.add_subscriber(iosub_4_1_1);
 
-	auto iopub_4_1_2 = std::make_shared<kaco::EntryPublisher>(device, "qry_motamps/channel_1");
-	bridge.add_publisher(iopub_4_1_2, loop_rate);
+	//auto iopub_4_1_2 = std::make_shared<kaco::EntryPublisher>(device, "qry_motamps/channel_1");
+	//bridge.add_publisher(iopub_4_1_2, loop_rate);
 	
-	auto iopub_4_1_3 = std::make_shared<kaco::EntryPublisher>(device, "qry_abcntr/channel_1");
-	bridge.add_publisher(iopub_4_1_3, loop_rate);
+	//auto iopub_4_1_3 = std::make_shared<kaco::EntryPublisher>(device, "qry_abcntr/channel_1");
+	//bridge.add_publisher(iopub_4_1_3, loop_rate);
 	
-	auto iopub_4_1_4 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_sencntr/counter_1");
-	bridge.add_subscriber(iopub_4_1_4);
+	//auto iopub_4_1_4 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_sencntr/counter_1");
+	//bridge.add_subscriber(iopub_4_1_4);
 	
 	
-	auto iosub_4_2_1 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_cango/cmd_cango_2");
-	bridge.add_subscriber(iosub_4_2_1);
+	//auto iosub_4_2_1 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_cango/cmd_cango_2");
+	//bridge.add_subscriber(iosub_4_2_1);
 
-	auto iopub_4_2_2 = std::make_shared<kaco::EntryPublisher>(device, "qry_motamps/channel_2");
-	bridge.add_publisher(iopub_4_2_2, loop_rate);
+	//auto iopub_4_2_2 = std::make_shared<kaco::EntryPublisher>(device, "qry_motamps/channel_2");
+	//bridge.add_publisher(iopub_4_2_2, loop_rate);
 	
-	auto iopub_4_2_3 = std::make_shared<kaco::EntryPublisher>(device, "qry_abcntr/channel_2");
-	bridge.add_publisher(iopub_4_2_3, loop_rate);
+	//auto iopub_4_2_3 = std::make_shared<kaco::EntryPublisher>(device, "qry_abcntr/channel_2");
+	//bridge.add_publisher(iopub_4_2_3, loop_rate);
 	
-	auto iopub_4_2_4 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_sencntr/counter_2");
-	bridge.add_subscriber(iopub_4_2_4);
+	//auto iopub_4_2_4 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_sencntr/counter_2");
+	//bridge.add_subscriber(iopub_4_2_4);
 	
 	
-	auto iosub_4_3_1 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_cango/cmd_cango_3");
-	bridge.add_subscriber(iosub_4_3_1);
+	//auto iosub_4_3_1 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_cango/cmd_cango_3");
+	//bridge.add_subscriber(iosub_4_3_1);
 
-	auto iopub_4_3_2 = std::make_shared<kaco::EntryPublisher>(device, "qry_motamps/channel_3");
-	bridge.add_publisher(iopub_4_3_2, loop_rate);
+	//auto iopub_4_3_2 = std::make_shared<kaco::EntryPublisher>(device, "qry_motamps/channel_3");
+	//bridge.add_publisher(iopub_4_3_2, loop_rate);
 	
-	auto iopub_4_3_3 = std::make_shared<kaco::EntryPublisher>(device, "qry_abcntr/channel_3");
-	bridge.add_publisher(iopub_4_3_3, loop_rate);
+	//auto iopub_4_3_3 = std::make_shared<kaco::EntryPublisher>(device, "qry_abcntr/channel_3");
+	//bridge.add_publisher(iopub_4_3_3, loop_rate);
 	
-	auto iopub_4_3_4 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_sencntr/counter_3");
-	bridge.add_subscriber(iopub_4_3_4);
-}
+	//auto iopub_4_3_4 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_sencntr/counter_3");
+	//bridge.add_subscriber(iopub_4_3_4);
+//}
 
 // initialize the topics for any Servo Cylinder actuator (must be 5.75" stroke length)
 void setupServoCylinderDevice(kaco::Device& device, kaco::Bridge& bridge, std::string& eds_files_path)
@@ -293,16 +291,16 @@ int main(int argc, char* argv[]) {
 	    setupMaxonDevice(device, bridge, eds_files_path);
 	}
 		
-		if (deviceId == 4)
-		{
-			device.load_dictionary_from_eds(eds_files_path + "roboteq_motor_controllers_v60.eds");
+		//if (deviceId == 4)
+		//{
+		//	device.load_dictionary_from_eds(eds_files_path + "roboteq_motor_controllers_v60.eds");
 			
-			ROS_DEBUG_STREAM("tfr_can: case: Device 4" << std::endl);
+		//	ROS_DEBUG_STREAM("tfr_can: case: Device 4" << std::endl);
 			
 			//device.print_dictionary();
 			
-			setupDevice4Topics(device, bridge, eds_files_path);
-		}
+		//	setupDevice4Topics(device, bridge, eds_files_path);
+		//}
 		
 		
 		else if (deviceId == 8)
@@ -346,100 +344,54 @@ int main(int argc, char* argv[]) {
 			
 		}
 		
-		else if (deviceId == 12)
-		{
-			device.load_dictionary_from_eds(eds_files_path + "roboteq_motor_controllers_v60.eds");
+		//else if (deviceId == 12)
+		//{
+			//device.load_dictionary_from_eds(eds_files_path + "roboteq_motor_controllers_v60.eds");
 			
-			ROS_DEBUG_STREAM("tfr_can: case: Device 12" << std::endl);
+			//ROS_DEBUG_STREAM("tfr_can: case: Device 12" << std::endl);
 			
 			
-			auto iosub_12_1_1 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_cango/cmd_cango_1");
-    		bridge.add_subscriber(iosub_12_1_1);
+			//auto iosub_12_1_1 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_cango/cmd_cango_1");
+    		//bridge.add_subscriber(iosub_12_1_1);
 
-			auto iopub_12_1_2 = std::make_shared<kaco::EntryPublisher>(device, "qry_motamps/channel_1");
-    		bridge.add_publisher(iopub_12_1_2, loop_rate);
+			//auto iopub_12_1_2 = std::make_shared<kaco::EntryPublisher>(device, "qry_motamps/channel_1");
+    		//bridge.add_publisher(iopub_12_1_2, loop_rate);
 			
-			auto iopub_12_1_3 = std::make_shared<kaco::EntryPublisher>(device, "qry_abcntr/channel_1");
-    		bridge.add_publisher(iopub_12_1_3, loop_rate);
+		//	auto iopub_12_1_3 = std::make_shared<kaco::EntryPublisher>(device, "qry_abcntr/channel_1");
+    		//bridge.add_publisher(iopub_12_1_3, loop_rate);
 			
-			auto iopub_12_1_4 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_sencntr/counter_1");
-			bridge.add_subscriber(iopub_12_1_4);
+		//	auto iopub_12_1_4 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_sencntr/counter_1");
+		//	bridge.add_subscriber(iopub_12_1_4);
 			
 			
-			auto iosub_12_2_1 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_cango/cmd_cango_2");
-    		bridge.add_subscriber(iosub_12_2_1);
+		//	auto iosub_12_2_1 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_cango/cmd_cango_2");
+    		//bridge.add_subscriber(iosub_12_2_1);
 
-			auto iopub_12_2_2 = std::make_shared<kaco::EntryPublisher>(device, "qry_motamps/channel_2");
-    		bridge.add_publisher(iopub_12_2_2, loop_rate);
+		//	auto iopub_12_2_2 = std::make_shared<kaco::EntryPublisher>(device, "qry_motamps/channel_2");
+    		//bridge.add_publisher(iopub_12_2_2, loop_rate);
 			
-			auto iopub_12_2_3 = std::make_shared<kaco::EntryPublisher>(device, "qry_abcntr/channel_2");
-    		bridge.add_publisher(iopub_12_2_3, loop_rate);
+		//	auto iopub_12_2_3 = std::make_shared<kaco::EntryPublisher>(device, "qry_abcntr/channel_2");
+    		//bridge.add_publisher(iopub_12_2_3, loop_rate);
 			
-			auto iopub_12_2_4 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_sencntr/counter_2");
-			bridge.add_subscriber(iopub_12_2_4);
+		//	auto iopub_12_2_4 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_sencntr/counter_2");
+		//	bridge.add_subscriber(iopub_12_2_4);
 			
 			
-			auto iosub_12_3_1 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_cango/cmd_cango_3");
-    		bridge.add_subscriber(iosub_12_3_1);
+		//	auto iosub_12_3_1 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_cango/cmd_cango_3");
+    		//bridge.add_subscriber(iosub_12_3_1);
 
-			auto iopub_12_3_2 = std::make_shared<kaco::EntryPublisher>(device, "qry_motamps/channel_3");
-    		bridge.add_publisher(iopub_12_3_2, loop_rate);
+		//	auto iopub_12_3_2 = std::make_shared<kaco::EntryPublisher>(device, "qry_motamps/channel_3");
+    		//bridge.add_publisher(iopub_12_3_2, loop_rate);
 			
-			auto iopub_12_3_3 = std::make_shared<kaco::EntryPublisher>(device, "qry_abcntr/channel_3");
-    		bridge.add_publisher(iopub_12_3_3, loop_rate);
+		//	auto iopub_12_3_3 = std::make_shared<kaco::EntryPublisher>(device, "qry_abcntr/channel_3");
+    		//bridge.add_publisher(iopub_12_3_3, loop_rate);
 			
-			auto iopub_12_3_4 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_sencntr/counter_3");
-			bridge.add_subscriber(iopub_12_3_4);
+		//	auto iopub_12_3_4 = std::make_shared<kaco::EntrySubscriber>(device, "cmd_sencntr/counter_3");
+		//	bridge.add_subscriber(iopub_12_3_4);
 			
-		}
+		//}
 		
-		else if (deviceId == IMU_NODE_ID)
-		{	
-			// Remember, we are not allowed to use the magnetometer. It is also disabled in the IMU's control settings, so no messages for that are being published on the can bus.
-	
-			device.load_dictionary_from_eds(eds_files_path + "LPMS-CU2_32BitDataSettings.eds");
-	
-			auto iopub_120_1 = std::make_shared<kaco::EntryPublisher>(device, "gyroscope_x");
-    		bridge.add_publisher(iopub_120_1, loop_rate);
-			
-			auto iopub_120_2 = std::make_shared<kaco::EntryPublisher>(device, "gyroscope_y");
-    		bridge.add_publisher(iopub_120_2, loop_rate);
-			
-			auto iopub_120_3 = std::make_shared<kaco::EntryPublisher>(device, "gyroscope_z");
-    		bridge.add_publisher(iopub_120_3, loop_rate);
-			
-			auto iopub_120_4 = std::make_shared<kaco::EntryPublisher>(device, "euler_x");
-    		bridge.add_publisher(iopub_120_4, loop_rate);
-			
-			auto iopub_120_5 = std::make_shared<kaco::EntryPublisher>(device, "euler_y");
-    		bridge.add_publisher(iopub_120_5, loop_rate);
-			
-			auto iopub_120_6 = std::make_shared<kaco::EntryPublisher>(device, "euler_z");
-    		bridge.add_publisher(iopub_120_6, loop_rate);
-			
-			auto iopub_120_7 = std::make_shared<kaco::EntryPublisher>(device, "linear_acceleration_x");
-    		bridge.add_publisher(iopub_120_7, loop_rate);
-			
-			auto iopub_120_8 = std::make_shared<kaco::EntryPublisher>(device, "linear_acceleration_y");
-    		bridge.add_publisher(iopub_120_8, loop_rate);
-			
-			auto iopub_120_9 = std::make_shared<kaco::EntryPublisher>(device, "linear_acceleration_z");
-    		bridge.add_publisher(iopub_120_9, loop_rate);
-			
-			auto iopub_120_10 = std::make_shared<kaco::EntryPublisher>(device, "quaternion_w");
-    		bridge.add_publisher(iopub_120_10, loop_rate);
-			
-			auto iopub_120_11 = std::make_shared<kaco::EntryPublisher>(device, "quaternion_x");
-    		bridge.add_publisher(iopub_120_11, loop_rate);
-			
-			auto iopub_120_12 = std::make_shared<kaco::EntryPublisher>(device, "quaternion_y");
-    		bridge.add_publisher(iopub_120_12, loop_rate);
-			
-			auto iopub_120_13 = std::make_shared<kaco::EntryPublisher>(device, "quaternion_z");
-    		bridge.add_publisher(iopub_120_13, loop_rate);
-			
-			
-		}
+
 	}
 	PRINT("About to call bridge.run()");
 	bridge.run();
