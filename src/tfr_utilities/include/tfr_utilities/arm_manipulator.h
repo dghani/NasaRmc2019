de1c60ff09b40d07c66dcea6f3e7550d9af522f3
Jhall118 <57575958+Jhall118@users.noreply.github.com>
2020-07-10 15:42:06 -0700

    Transfered code from Jons working arm branch

--- src/tfr_utilities/include/tfr_utilities/arm_manipulator.h
+++ src/tfr_utilities/include/tfr_utilities/arm_manipulator.h
@@ -8,6 +8,7 @@
 #include <joints.h>
 #include <urdf/model.h>
 #include <actionlib/client/simple_action_client.h>
+#include <sensor_msgs/JointState.h>
 
 /**
  * Provides a simple method for moving the arm without MoveIt.
@@ -23,38 +24,48 @@ class ArmManipulator
         ArmManipulator(ArmManipulator&&)=delete;
         ArmManipulator& operator=(ArmManipulator&&)=delete;
 
-        /**
+		/**
          * Moves the arm to the given position.
-         *
-         * Notes:
-         *  - Careful what parameters are passed in, the arm could collide with the robot.
-         *
-         *  - The method is not blocking, so the caller needs to wait for the arm to move.
-         *    See digging_action_server.cpp for example.
+		 *
+		 * Notes:
+		 *  - Careful what parameters are passed in, the arm could collide with the robot.
+		 *
+		 *  - The method is not blocking, so the caller needs to wait for the arm to move.
+		 *    See digging_action_server.cpp for example.
          * */
         void moveArm( const double& turntable, const double& lower_arm, const double& upper_arm, const double& scoop);
 
-        /*
-         * Same as moveArm but clamps trajectories to be within the URDF model's joint limits.
-         */
-        void moveArmWithLimits(const double& turntable, const double& lower_arm ,const double& upper_arm,  const double& scoop );
+		/*
+		 * Same as moveArm but clamps trajectories to be within the URDF model's joint limits.
+		 */
+		void moveArmWithLimits(const double& turntable, const double& lower_arm ,const double& upper_arm,  const double& scoop );
 
 
         void moveArmWithoutPlanningOrLimits(
             const double& turntable, const double& lower_arm, const double& upper_arm, const double& scoop);
-        
+
     private:
         ros::Publisher trajectory_publisher;
         ros::Publisher scoop_trajectory_publisher;
+
+        // These publishers do NOT go through ros_control.
+        // They send messages to the topics created bt the can_bus node in tfr_can.
+        ros::Publisher lower_arm_publisher;
+        ros::Publisher upper_arm_publisher;
+        ros::Publisher scoop_publisher;
+
         actionlib::SimpleActionClient<tfr_msgs::ArmMoveAction> arm_action_client;
 
-        void initializeJointLimits();
+		void initializeJointLimits();
 
-        // Return input, restricted to be within the two bounds (inclusive).
-        double clamp(double input, double bound_1, double bound_2);
+		// Return input, restricted to be within the two bounds (inclusive).
+		double clamp(double input, double bound_1, double bound_2);
 
-        double lower_limits[tfr_utilities::Joint::JOINT_COUNT] = {0};
-        double upper_limits[tfr_utilities::Joint::JOINT_COUNT] = {0};
+		double lower_limits[tfr_utilities::Joint::JOINT_COUNT] = {0};
+		double upper_limits[tfr_utilities::Joint::JOINT_COUNT] = {0};
+
+        trajectory_msgs::JointTrajectory createSinglePointArmTrajectory(const double turntable, const double lower_arm, const double upper_arm);
+        trajectory_msgs::JointTrajectory createSinglePointArmEndTrajectory(const double scoop);
  };
 
 #endif
