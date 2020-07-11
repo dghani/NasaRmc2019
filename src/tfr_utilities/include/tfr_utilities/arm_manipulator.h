d62035b20e3a6da3dcb324fa9253983f4914d875
Daniel Mamaghani <daniel.mamaghani@gmail.com>
2020-07-10 22:16:38 -0700

    Add new methods for moving arm actuators individually. Remove dead code in arm_manipulator.cpp.

--- src/tfr_utilities/include/tfr_utilities/arm_manipulator.h
+++ src/tfr_utilities/include/tfr_utilities/arm_manipulator.h
@@ -11,7 +11,7 @@
 #include <sensor_msgs/JointState.h>
 
 /**
- * Provides a simple method for moving the arm without MoveIt.
+ * Provides a simple method for moving the arm.
  * This is a regular ole' class, just instantiate it and call moveArm.
  * */
 class ArmManipulator
@@ -24,49 +24,21 @@ class ArmManipulator
         ArmManipulator(ArmManipulator&&)=delete;
         ArmManipulator& operator=(ArmManipulator&&)=delete;
 
-		/**
-         * Moves the arm to the given position.
-		 *
-		 * Notes:
-		 *  - Careful what parameters are passed in, the arm could collide with the robot.
-		 *
-		 *  - The method is not blocking, so the caller needs to wait for the arm to move.
-		 *    See digging_action_server.cpp for example.
-         * */
         void moveArm( const double& turntable, const double& lower_arm, const double& upper_arm, const double& scoop);
-
-		/*
-		 * Same as moveArm but clamps trajectories to be within the URDF model's joint limits.
-		 */
-		void moveArmWithLimits(const double& turntable, const double& lower_arm ,const double& upper_arm,  const double& scoop );
-
 
         void moveArmWithoutPlanningOrLimits(
             const double& turntable, const double& lower_arm, const double& upper_arm, const double& scoop);
 
-    private:
-        ros::Publisher trajectory_publisher;
-        ros::Publisher scoop_trajectory_publisher;
+        void moveTurntablePosition(double turntable);
+        void moveLowerArmPosition(double lower_arm);
+        void moveUpperArmPosition(double upper_arm);
+        void moveScoopPosition(double scoop);
 
-        // These publishers do NOT go through ros_control.
-        // They send messages to the topics created bt the can_bus node in tfr_can.
+    private:
         ros::Publisher turntable_publisher;
         ros::Publisher lower_arm_publisher;
         ros::Publisher upper_arm_publisher;
         ros::Publisher scoop_publisher;
-
-        actionlib::SimpleActionClient<tfr_msgs::ArmMoveAction> arm_action_client;
-
-		void initializeJointLimits();
-
-		// Return input, restricted to be within the two bounds (inclusive).
-		double clamp(double input, double bound_1, double bound_2);
-
-		double lower_limits[tfr_utilities::Joint::JOINT_COUNT] = {0};
-		double upper_limits[tfr_utilities::Joint::JOINT_COUNT] = {0};
-
-        trajectory_msgs::JointTrajectory createSinglePointArmTrajectory(const double turntable, const double lower_arm, const double upper_arm);
-        trajectory_msgs::JointTrajectory createSinglePointArmEndTrajectory(const double scoop);
  };
 
 #endif
--- src/tfr_utilities/src/arm_manipulator.cpp
+++ src/tfr_utilities/src/arm_manipulator.cpp
@@ -1,211 +1,82 @@
 #include <arm_manipulator.h>
 
 ArmManipulator::ArmManipulator(ros::NodeHandle &n, bool init_joints):
-            arm_action_client{n, "move_arm"},
-            trajectory_publisher{n.advertise<std_msgs::Float64MultiArray>("/arm_controller/command", 5)},
-            scoop_trajectory_publisher{n.advertise<std_msgs::Float64MultiArray>("/arm_end_controller/command", 5)},
             turntable_publisher{n.advertise<sensor_msgs::JointState>("/device1/set_joint_state", 5)},
             lower_arm_publisher{n.advertise<sensor_msgs::JointState>("/device23/set_joint_state", 5)},
             upper_arm_publisher{n.advertise<sensor_msgs::JointState>("/device45/set_joint_state", 5)},
             scoop_publisher{n.advertise<sensor_msgs::JointState>("/device56/set_joint_state", 5)}
 {
   ROS_INFO("Initializing Arm Manipulator");
-	if (init_joints) initializeJointLimits();
-  ROS_ERROR("Beep");
-}
-
-void ArmManipulator::initializeJointLimits()
-{
-	// Get the model description
-    std::string desc;
-    if (!ros::param::get("/robot_description", desc)) {desc = "";}
-
-    if (desc.length() == 0)
-    {
-        ROS_WARN("robot_description is empty and controller_launcher is using fake values, quitting.");
-        return;
-    }
-
-    urdf::Model model;
-    if (!model.initString(desc))
-    {
-        ROS_WARN("Couldn't load robot_description.");
-        return;
-    }
-
-    ROS_INFO("Model loaded successfully, loading joint limits.");
-    const int joint_count = 5;
-    using namespace tfr_utilities;
-    int joint_enum_array[joint_count] = {Joint::BIN, Joint::LOWER_ARM,
-      Joint::UPPER_ARM, Joint::SCOOP};
-    std::string joint_name_array[joint_count] = {"bin_joint", "lower_arm_joint",
-      "upper_arm_joint", "scoop_joint"};
-    for (int i = 0; i < joint_count; i++){
-      auto joint_name = joint_name_array[i];
-      auto joint_ptr = model.getJoint(joint_name);
-      if (!joint_ptr){
-        ROS_WARN_STREAM("Could not locate joint " << joint_name << " on model to access limits");
-        continue;
-      }
-      auto limits = joint_ptr->limits;
-      lower_limits[joint_enum_array[i]] = limits->lower;
-      upper_limits[joint_enum_array[i]] = limits->upper;
-    }
 }
 
 void ArmManipulator::moveArm(const double& turntable, const double& lower_arm ,const double& upper_arm,  const double& scoop )
 {
-    ROS_INFO_STREAM("Arm manipulator called by: " << ros::this_node::getName() << ". Parameters: " << turntable << ", " << lower_arm << ", " << upper_arm << ", " << scoop << std::endl);
+    ROS_INFO_STREAM("Arm manipulator moveArm() called by: " << 
+    ros::this_node::getName() << ". Parameters: " 
+<< turntable << ", " << lower_arm << ", " << upper_arm << ", " << scoop << std::endl);
+    ROS_INFO_STREAM("ArmManipulator::moveArm() deprecated. Replace calls to moveArm()." << std::endl);
 
-    tfr_msgs::ArmMoveGoal goal{};
-    goal.pose.resize(4);
-
-    goal.pose[0] = turntable;
-    goal.pose[1] = lower_arm;
-    goal.pose[2] = upper_arm;
-    goal.pose[3] = scoop;
-
-    arm_action_client.sendGoal(goal);
-    while ( !arm_action_client.getState().isDone() && ros::ok())
-    {
-        if (! ros::ok())
-        {
-            arm_action_client.cancelAllGoals();
-            ROS_INFO("Arm Manip: exiting");
-            return;
-        }
-    }
-
-    if (arm_action_client.getState()!=actionlib::SimpleClientGoalState::SUCCEEDED)
-    {
-        ROS_INFO("Arm Manip: arm move failed");
-        return;
-    }
+    // TODO: Replace the other references to moveArm() in the code base.
 }
 
-// Asynchronous call.
+void ArmManipulator::moveTurntablePosition(double turntable)
+{
+    sensor_msgs::JointState turntable_joint_state;
+
+    turntable_joint_state.header.stamp = ros::Time::now();
+    turntable_joint_state.position.push_back(turntable);
+    
+    turntable_publisher.publish(turntable_joint_state);
+}
+
+void ArmManipulator::moveLowerArmPosition(double lower_arm)
+{
+    sensor_msgs::JointState lower_arm_joint_state;
+
+    lower_arm_joint_state.header.stamp = ros::Time::now();
+    lower_arm_joint_state.position.push_back(lower_arm);
+
+    lower_arm_publisher.publish(lower_arm_joint_state);
+}
+
+void ArmManipulator::moveUpperArmPosition(double upper_arm)
+{
+    sensor_msgs::JointState upper_arm_joint_state;
+
+    upper_arm_joint_state.header.stamp = ros::Time::now();
+    upper_arm_joint_state.position.push_back(upper_arm);
+
+    upper_arm_publisher.publish(upper_arm_joint_state);
+}
+
+void ArmManipulator::moveScoopPosition(double scoop)
+{
+    sensor_msgs::JointState scoop_joint_state;
+
+    scoop_joint_state.header.stamp = ros::Time::now();
+    scoop_joint_state.position.push_back(scoop);
+
+    scoop_publisher.publish(scoop_joint_state);
+}
+
+/*
+ * Moves the arm to the given position.
+ *
+ * Notes:
+ *  - Careful what parameters are passed in, the arm could collide with the robot.
+ *
+ *  - The method is not blocking, so the caller needs to wait for the arm to move.
+ *    See digging_action_server.cpp for example.
+ */
 void ArmManipulator::moveArmWithoutPlanningOrLimits(
             const double& turntable, const double& lower_arm, const double& upper_arm, const double& scoop)
 {
     ROS_INFO_STREAM("moveArmWithoutPlanningOrLimits() called by: " << ros::this_node::getName() << ". Parameters: " << turntable << ", " << lower_arm << ", " << upper_arm << ", " << scoop << std::endl);
 
-    sensor_msgs::JointState turntable_joint_state;
-    sensor_msgs::JointState lower_arm_joint_state;
-    sensor_msgs::JointState upper_arm_joint_state;
-    sensor_msgs::JointState scoop_joint_state;
-
-    auto current_time = ros::Time::now();
-
-    turntable_joint_state.header.stamp = current_time;
-    turntable_joint_state.position.push_back(turntable);
-
-    lower_arm_joint_state.header.stamp = current_time;
-    lower_arm_joint_state.position.push_back(lower_arm);
-
-    upper_arm_joint_state.header.stamp = current_time;
-    upper_arm_joint_state.position.push_back(upper_arm);
-
-    scoop_joint_state.header.stamp = current_time;
-    scoop_joint_state.position.push_back(scoop);
-
-    turntable_publisher.publish(turntable_joint_state);
-    lower_arm_publisher.publish(lower_arm_joint_state);
-    upper_arm_publisher.publish(upper_arm_joint_state);
-    scoop_publisher.publish(scoop_joint_state);
+    moveTurntablePosition(turntable);
+    moveLowerArmPosition(lower_arm);
+    moveUpperArmPosition(upper_arm);
+    moveScoopPosition(scoop);
 
     return;
-}
-
-// TODO: Check for NaN input.
-double ArmManipulator::clamp(double input, double bound_1, double bound_2)
-{
-	double lower_bound = 0;
-	double upper_bound = 0;
-
-	if ( bound_1 <= bound_2 )	{
-		lower_bound = bound_1;
-		upper_bound = bound_2;
-	} else {
-		lower_bound = bound_2;
-		upper_bound = bound_1;
-	}
-	return std::max(std::min(input, upper_bound), lower_bound);
-}
-
-trajectory_msgs::JointTrajectory
-ArmManipulator::createSinglePointArmTrajectory(
-    const double turntable, const double lower_arm, const double upper_arm)
-{
-    const int NUM_ARM_TRAJ_JOINTS = 3;
-
-    trajectory_msgs::JointTrajectory arm_traj;
-
-    arm_traj.joint_names.resize(NUM_ARM_TRAJ_JOINTS); // very important to resize fisrt, otherwise data will not be populated.
-    arm_traj.joint_names[0] = "turntable_joint";
-    arm_traj.joint_names[1] = "lower_arm_joint";
-    arm_traj.joint_names[2] = "upper_arm_joint";
-    
-    arm_traj.points.resize(1); // publish a single waypoint. A trajectory can have multiple.
-    
-    arm_traj.points[0].positions.resize(NUM_ARM_TRAJ_JOINTS);
-    arm_traj.points[0].positions[0] = turntable;
-    arm_traj.points[0].positions[1] = lower_arm;
-    arm_traj.points[0].positions[2] = upper_arm;
-    
-    // tell the arm to stop at the waypoint
-    arm_traj.points[0].velocities.resize(NUM_ARM_TRAJ_JOINTS);
-    arm_traj.points[0].velocities[0] = 0;
-    arm_traj.points[0].velocities[1] = 0;
-    arm_traj.points[0].velocities[2] = 0;
-
-    // Dunno if we need this one.
-    arm_traj.points[0].time_from_start = ros::Duration(1.0); 
-
-    arm_traj.header.stamp = ros::Time::now();
-
-    return arm_traj;
-}
-
-trajectory_msgs::JointTrajectory
-ArmManipulator::createSinglePointArmEndTrajectory(const double scoop)
-{
-    const int NUM_ARM_END_TRAJ_JOINTS = 1;
-
-    trajectory_msgs::JointTrajectory arm_end_traj;
-
-    arm_end_traj.joint_names.resize(NUM_ARM_END_TRAJ_JOINTS);
-    arm_end_traj.joint_names[0] = "scoop_joint";
-    
-    arm_end_traj.points.resize(1); 
-    
-    arm_end_traj.points[0].positions.resize(NUM_ARM_END_TRAJ_JOINTS);
-    arm_end_traj.points[0].positions[0] = scoop;
-    
-    arm_end_traj.points[0].velocities.resize(NUM_ARM_END_TRAJ_JOINTS);
-    arm_end_traj.points[0].velocities[0] = 0;
-
-    arm_end_traj.points[0].time_from_start = ros::Duration(1.0); 
-
-    arm_end_traj.header.stamp = ros::Time::now();
-
-    return arm_end_traj;
-}
-
-void ArmManipulator::moveArmWithLimits(const double& turntable, const double& lower_arm ,const double& upper_arm,  const double& scoop )
-{
-	double clamp_turntable = turntable; // no joint limits on turntable
-
-	double clamp_lower_arm = clamp(lower_arm,
-				lower_limits[static_cast<int>(tfr_utilities::Joint::LOWER_ARM)],
-				upper_limits[static_cast<int>(tfr_utilities::Joint::LOWER_ARM)]);
-
-	double clamp_upper_arm = clamp(upper_arm,
-				lower_limits[static_cast<int>(tfr_utilities::Joint::UPPER_ARM)],
-				upper_limits[static_cast<int>(tfr_utilities::Joint::UPPER_ARM)]);
-
-	double clamp_scoop = clamp(scoop,
-				lower_limits[static_cast<int>(tfr_utilities::Joint::SCOOP)],
-				upper_limits[static_cast<int>(tfr_utilities::Joint::SCOOP)]);
-
-	moveArm(clamp_turntable, clamp_lower_arm, clamp_upper_arm, clamp_scoop);
 }
