df7791ace6cd270441ddbca173f159ed041d7152
Suhyun Bae <sbaehyun@gmail.com>
2020-09-05 17:10:23 -0700

    Added node for the front cam

--- start_robot.sh
+++ start_robot.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 export ROS_MASTER_URI=http://127.0.0.1:11311
-export ROS_HOSTNAME=$(hostname -I | tr -d "[:space:]")
+export ROS_HOSTNAME=192.168.0.11
 echo "Attempting to bring down CAN interfaces"
 ./shutdownCAN.sh
 echo "Attempting to bring up CAN interfaces"
 ./setupCAN.sh
 . devel/setup.bash
 roslaunch tfr_launch robot.launch
+
+
