d55a799aa67819dd30ad4f4c0414a31cfbb315e5
Jon Hall <Jonhall19@gmail.com>
2020-09-06 23:24:50 -0700

    Drivebase_odom Publisher updates

--- src/tfr_sensor/src/drivebase_odom_publisher.cpp
+++ src/tfr_sensor/src/drivebase_odom_publisher.cpp
@@ -143,7 +143,7 @@ class DrivebaseOdometryPublisher
         msg.pose.pose.position.x = x;
         msg.pose.pose.position.y = y;
         msg.pose.pose.position.z = 0;
-        msg.pose.pose.orientation = yaw; 
+        msg.pose.pose.orientation = double yaw; 
         msg.pose.covariance = { 
             1e-1, 0,    0,    0,    0,    0,
             0, 1e-1,    0,    0,    0,    0,
