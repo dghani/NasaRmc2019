3d429ff46a2be44fcc73e68c949be7cbaf15cfd2
trickfire <Jonhall19@gmail.com>
2020-09-14 15:03:10 -0700

    Changes to realsense setup and OpenCV configuration

--- install_librealsense.sh
+++ install_librealsense.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-version="v2.24.0"
-rm -rf librealsense
-git clone https://github.com/IntelRealSense/librealsense.git
-cd librealsense
-git checkout $version
-sudo apt-get install git libssl-dev libusb-1.0-0-dev pkg-config libgtk-3-dev libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev
-./scripts/setup_udev_rules.sh
-./scripts/patch-realsense-ubuntu-lts.sh
-mkdir build; cd build;
-cmake ../ -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=true -DBUILD_GRAPHICAL_EXAMPLES=true
-sudo make uninstall && make clean && make -j8 && sudo make install
+rm -rf installRealSenseSDK
+git clone https://github.com/jetsonhacks/installRealSenseSDK.git
+cd installRealSenseSDK
+sudo apt-get update
+sudo apt-get install libssl-dev libusb-1.0-0-dev pkg-config -y
+sudo apt-get install build-essential cmake cmake-curses-gui -y
+sudo apt-get install libgtk-3-dev libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev -y
+sudo apt-get install qtcreator -y
+sudo apt-get install -y python3 python3-dev
+
+./buildLibrealsense.sh
+
