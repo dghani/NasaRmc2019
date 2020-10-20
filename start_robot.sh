2f6578036670c978013bce90555ce3b46f1517ea
Jon Hall <Jonhall19@gmail.com>
2020-10-19 19:09:42 -0700

    Fixing start_robot.sh networking on actual hardware.  This might break other people's builds

--- start_robot.sh
+++ start_robot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export ROS_MASTER_URI=http://127.0.0.1:11311
-export ROS_HOSTNAME=$(hostname -I | tr -d "[:space:]")
+export ROS_MASTER_URI=http://192.168.0.181:11311
+export ROS_HOSTNAME=192.168.0.181
 echo "Attempting to bring down CAN interfaces"
 ./shutdownCAN.sh
 echo "Attempting to bring up CAN interfaces"
