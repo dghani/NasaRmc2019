2d40a808eae5f7ab68a36bceebf87c336931cf92
Jhall118 <57575958+Jhall118@users.noreply.github.com>
2020-12-31 13:06:05 -0800

    Update aruco_action_server.cpp

--- src/tfr_aruco/src/aruco_action_server.cpp
+++ src/tfr_aruco/src/aruco_action_server.cpp
@@ -135,7 +135,7 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "aruco_action_server");
     ros::NodeHandle n{};
     TFR_Aruco aruco{n};
-    ros::Rate rate(12);
+    ros::Rate rate(6);
     while(ros::ok()){
         ros::spinOnce();
         rate.sleep();
