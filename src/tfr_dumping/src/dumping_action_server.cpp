96928ca6504337f59d75698b605942c12edc96e0
Jhall118 <57575958+Jhall118@users.noreply.github.com>
2021-03-27 13:10:47 -0700

    Update dumping_action_server.cpp

--- src/tfr_dumping/src/dumping_action_server.cpp
+++ src/tfr_dumping/src/dumping_action_server.cpp
@@ -110,7 +110,7 @@
 
        void drivebaseOdomCallback(const nav_msgs::Odometry& treadDistance) {
          // if first time through this callback will set original position
-         if (!detector.getState().isDone() && true) {
+         if (!detector.getState().isDone()) {
            originalTreadDistance = treadDistance.pose.pose.position.x;
            ROS_INFO("yeet");
            isTreadFirst++;
