3d935c02c597a935147dceb66404a5c1e6aed471
BradenWolf9 <72713649+BradenWolf9@users.noreply.github.com>
2021-03-31 12:37:58 -0700

    Update monitoring_battery.cpp

--- src/tfr_monitoring/src/monitoring_battery.cpp
+++ src/tfr_monitoring/src/monitoring_battery.cpp
@@ -2,10 +2,10 @@
  * File:    monitoring_battery.cpp
  * Node:    monitoring_battery
  * 
- * Purpose: This is a subscriber that will listen to the current output while the arm is moving 
- *			to make sure that it does not exceed the capacity of the controllers
- *			To help tell the digging action server to change the digging queue becausethe current one
- *			is using to much power
+ * Purpose: This is a subscriber that will listen to the voltage output of the battery. When the 
+ *	    battery is outputting		
+ *			
+ *			
  *
  *	TO_DO: not finished yet, might not use for this years compition 
  ****************************************************************************************/
@@ -16,9 +16,10 @@
 
 
 void batteryVoltageCallback(const std_msgs::UInt16& batteryVoltage) {
-	//Roboteq controller sends voltage * 10 back i.e. if 150 is reported than it is 15 volts
+	//Roboteq controller sends voltage * 10 back i.e. if 150 is reported than it is 15 volts.
+	//What is reported is diveded by 10 to get the actual voltage.
         int batteryVolt = batteryVoltage.data / 10;
-        //if battery voltage is below 30 than it needs to be charged
+        //if battery voltage is below 37 than it needs to be charged
         if (batteryVolt < 37) {
         	ROS_ERROR("BATTERY LOW! CHARGE NOW! (%d volts)\n", batteryVolt);
         }
