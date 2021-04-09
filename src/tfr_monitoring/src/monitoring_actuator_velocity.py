44aad333d2f9f5fd13e8cec85285a0b1392ff7db
Braden Wolf <bwolf1@uw.edu>
2021-04-09 13:25:59 -0700

    created monitoring_actuator_velocity.py

---
+++ src/tfr_monitoring/src/monitoring_actuator_velocity.py
@@ -0,0 +1,10 @@
+##########################################################################################
+# File:    monitoring_actuator_velocity.py
+# Node:    monitoring_actuator_velocity
+#
+# Purpose: This subscribes to the velocities of the actuators to determine when they
+#          are done moving. This also publishes a bool for each actuator depending on
+#          whether they are moving or not dependent on the tolerances set in the launch
+#          file.
+#
+##########################################################################################
