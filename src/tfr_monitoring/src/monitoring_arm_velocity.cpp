402df6188c47d0d0077263ae41037b533817198c
Jhall118 <57575958+Jhall118@users.noreply.github.com>
2021-04-24 15:25:00 -0700

    Update monitoring_arm_velocity.cpp

--- src/tfr_monitoring/src/monitoring_arm_velocity.cpp
+++ src/tfr_monitoring/src/monitoring_arm_velocity.cpp
@@ -71,6 +71,7 @@ private:
     else {
       this->turnTableMoving.data = true;
     }
+    ROS_INFO("Turn table velocity: %d\n"turnTableVelocity.data)
     turnTablePub.publish(turnTableMoving);
   }
 
