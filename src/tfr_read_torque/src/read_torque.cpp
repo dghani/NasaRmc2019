878fad472169f55de6e6edfd4eba1f8ad8860418
VladFigy <vladimirkupryukhin@gmail.com>
2021-12-19 09:53:24 -0800

    Implement basic c++ code

--- src/tfr_read_torque/src/read_torque.cpp
+++ src/tfr_read_torque/src/read_torque.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <ros/ros.h>
 
-int main() {
+class read_torque {
+
+public:
+	readTorqueValues(ross:NodeHandle &handler) : 
+		lowerArmSub{handler.subscribe("/device23/get_torque_actual_value")}
+	{}
+
+//class variables
+private:
+	ros::Subscriber lowerArmSub;
+
+
+
+
+//callback functions
+	void lowerArmCallback() {
+		std::cout << "Lower arm callback"
+
+	}
+};
+
+
+
+//Entry point
+int main(int argc, char** argv) {
+	ros::init(argc, argv, "read_torque");
+	ros::NodeHandle nodeHandler;
+
+	readTorqueValues(nodeHandler);
+
+	ros::Rate loop_rate(10);
+	ros::spin();
+
+
 	std::cout << "Hello World";
 	return 0;
 }
