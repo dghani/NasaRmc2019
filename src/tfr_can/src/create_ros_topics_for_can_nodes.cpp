e8b2b169b4c953306c80e9848d504fc351704c22
Jhall118 <57575958+Jhall118@users.noreply.github.com>
2022-05-20 12:44:38 -0700

    Update create_ros_topics_for_can_nodes.cpp

--- src/tfr_can/src/create_ros_topics_for_can_nodes.cpp
+++ src/tfr_can/src/create_ros_topics_for_can_nodes.cpp
@@ -129,10 +129,10 @@ void setupMaxonDevice(kaco::Device& device, kaco::Bridge& bridge, std::string& e
 
     // min: 0 -> 0, 
     // max: 16 hall clicks * 4.3 Maxon gear * 34 worm gear = 4093   
-    auto jspub = std::make_shared<kaco::JointStatePublisher>(device, -4093, 4093); 
+    auto jspub = std::make_shared<kaco::JointStatePublisher>(device, -8186, 8186); 
     bridge.add_publisher(jspub, loop_rate);
     
-    auto jssub = std::make_shared<kaco::JointStateSubscriber>(device, -4093, 4093); 
+    auto jssub = std::make_shared<kaco::JointStateSubscriber>(device, -8186, 8186); 
     bridge.add_subscriber(jssub);		
 
     // Read the "statusword" from the CANopen device.
